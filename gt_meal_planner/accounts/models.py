711e5568714f89b978e9b9b8f3235675866451db
mwittland <mwittland8@gmail.com>
2025-04-02 22:50:02 -0400

    created profile and meal plan models

---
+++ gt_meal_planner/accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    maiden_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.user.username
+
+class MealPlan(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="meal_plan")
+    meal_swipes = models.IntegerField(default=0)
+    dining_dollars = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.profile.user.username + "'s Meal Plan"
+    
