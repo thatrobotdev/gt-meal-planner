23bf0e520dcd9486d982e9ce8f5b848bae20b280
mwittland <mwittland8@gmail.com>
2025-04-02 21:01:52 -0400

    updated about and home page

--- home/views.py
+++ home/views.py
@@ -4,11 +4,9 @@ from movies.models import Movie
 
 def index(request):
     movies = list(Movie.objects.all()) 
-    random_movies = random.sample(movies, min(3, len(movies)))
 
     template_data = {}
     template_data['title'] = 'Movies Store'
-    template_data['random_movies'] = random_movies
     return render(request, 'home/index.html', {'template_data': template_data})
 
 def about(request):
