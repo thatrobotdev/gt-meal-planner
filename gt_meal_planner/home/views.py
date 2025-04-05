43ad7ea7709b75d60c0b066665f09b7deae10cb9
mwittland <mwittland8@gmail.com>
2025-04-05 12:45:02 -0400

    added basic user meal plan info to home page

--- gt_meal_planner/home/views.py
+++ gt_meal_planner/home/views.py
@@ -1,10 +1,17 @@
 import random
 from django.shortcuts import render
+from accounts.models import MealPlan
 
 def index(request):
-
+    user = request.user
     template_data = {}
-    template_data['title'] = 'Movies Store'
+    if user.is_authenticated:
+        meal_plan = MealPlan.objects.get(user=user)
+        template_data['username'] = user.username
+        template_data['swipes'] = meal_plan.meal_swipes
+        template_data['dollars'] = meal_plan.dining_dollars
+        template_data['start'] = meal_plan.start_date
+        template_data['end'] = meal_plan.end_date
     return render(request, 'home/index.html', {'template_data': template_data})
 
 def about(request):
